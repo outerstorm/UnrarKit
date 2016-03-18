809cb4ed0cb4b30e2d66a7b2816d605ccf5ea311
Dov Frankel <dov@abbey-code.com>
2016-03-17 23:44:48 -0400

    Updated script output to include a couple of newlines

--- Scripts/push-output.sh
+++ Scripts/push-output.sh
@@ -6,7 +6,7 @@ $(npm bin)/travis-after-all
 exitCode=$?
 
 if [ $exitCode -eq 0 ]; then
-    echo "Build succeeded"
+    echo -e "\n\nBuild succeeded"
 else
-    echo "Build not done yet, or failed"
+    echo -e "\n\nBuild not done yet, or failed"
 fi
