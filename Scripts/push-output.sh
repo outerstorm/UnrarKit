1434f73202bc34369b26eb9193c30a10c41faf1f
Dov Frankel <dov@abbey-code.com>
2016-03-18 00:10:49 -0400

    Completed push-output.sh

--- Scripts/push-output.sh
+++ Scripts/push-output.sh
@@ -5,8 +5,28 @@ declare exitCode;
 $(npm bin)/travis-after-all
 exitCode=$?
 
-if [ $exitCode -eq 0 ]; then
-    echo -e "\n\nBuild succeeded"
-else
-    echo -e "\n\nBuild not done yet, or failed"
+if [ $exitCode -ne 0 ]; then
+    echo -e "\n\nAll builds not done yet, or failed"
+    exit 0
 fi
+
+if [ -z "$TRAVIS_TAG" ]; then
+    echo -e "\nBuild is not tagged"
+    exit 0
+fi
+
+pod spec lint --fail-fast
+
+if [ $? -ne 0 ]; then
+    echo -e "\nPodspec failed lint"
+    exit 0
+fi
+
+# Make non-master builds pre-release
+if [ "$TRAVIS_BRANCH" -ne "master" ] && [[ $TRAVIS_TAG != *"beta"* ]]; then
+    echo -e "\nBranch build not tagged with 'beta'"
+    exit 0
+fi
+
+echo -e "\nPushing to CocoaPods\n"
+#pod trunk push
