7db5068021464e8723d66ac814034e246ef865ad
Dov Frankel <dov@abbey-code.com>
2016-03-23 10:10:28 -0400

    Allow this script to run, even if its changes have not been committed [ci skip]

--- Scripts/set-version.sh
+++ Scripts/set-version.sh
@@ -4,8 +4,12 @@
 #
 # Updates the main plist file, then tags the build in Git, using the release notes from CHANGELOG.md
 
-# Only continue if the repo has no changes
-CHANGED_FILE_COUNT=$(git status --porcelain --untracked-files=no | wc -l)
+# Did this script change since the last commit?
+THIS_FILE_REGEX='.*Scripts/set-version\.sh.*'
+THIS_FILE_CHANGED=$(git status --porcelain --untracked-files=no | grep $THIS_FILE_REGEX | wc -l)
+
+# Only continue if the repo has no changes (excluding this script)
+CHANGED_FILE_COUNT=$(git status --porcelain --untracked-files=no | grep -v $THIS_FILE_REGEX | wc -l)
 if [ $CHANGED_FILE_COUNT -gt 0 ]; then
     echo "Please commit or discard any changes before continuing"
     exit 1
@@ -31,6 +35,11 @@ fi
 echo "Updating version numbers in plist to '$1'.."
 agvtool new-version -all "$1" # CFBundleVersion
 agvtool new-marketing-version "$1" # CFBundleShortVersionString
+
+if [ "$THIS_FILE_CHANGED" -gt 0 ]; then
+    echo -e "\n\n\033[1;33mNot committing to Git, as this script isn't final. Commit it to continue\033[0m"
+    exit 2
+fi
 
 echo "Committing updated plist..."
 git commit -m "Updated plist to v$1" Resources
