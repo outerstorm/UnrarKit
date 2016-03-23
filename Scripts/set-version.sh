2a25a4c66e25e25bc6bd23ed2205db986b725c33
Dov Frankel <dov@abbey-code.com>
2016-03-23 10:22:26 -0400

    Added color to output [ci skip]

--- Scripts/set-version.sh
+++ Scripts/set-version.sh
@@ -4,6 +4,12 @@
 #
 # Updates the main plist file, then tags the build in Git, using the release notes from CHANGELOG.md
 
+# Colors
+COLOR_OFF='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD_YELLOW='\033[1;33m'
+
 # Did this script change since the last commit?
 THIS_FILE_REGEX='.*Scripts/set-version\.sh.*'
 THIS_FILE_CHANGED=$(git status --porcelain --untracked-files=no | grep $THIS_FILE_REGEX | wc -l)
@@ -11,13 +17,13 @@ THIS_FILE_CHANGED=$(git status --porcelain --untracked-files=no | grep $THIS_FIL
 # Only continue if the repo has no changes (excluding this script)
 CHANGED_FILE_COUNT=$(git status --porcelain --untracked-files=no | grep -v $THIS_FILE_REGEX | wc -l)
 if [ $CHANGED_FILE_COUNT -gt 0 ]; then
-    echo "Please commit or discard any changes before continuing"
+    echo -e "${RED}Please commit or discard any changes before continuing$COLOR_OFF"
     exit 1
 fi
 
 # Require a single argument to be passed in
 if [ "$#" -ne 1 ]; then
-    echo "Please pass the desired version number as an argument"
+    echo -e "${RED}Please pass the desired version number as an argument$COLOR_OFF"
     exit 1
 fi
 
@@ -28,24 +34,24 @@ RELEASE_VERSION=${BASH_REMATCH[1]}
 # Require release notes to be written
 RELEASE_NOTES=$(sed "/^## $RELEASE_VERSION$/,/^##/!d;//d;/^$/d" CHANGELOG.md)
 if [ -z "$RELEASE_NOTES" ]; then
-    echo "Please add release notes for v$1 into CHANGELOG.md"
+    echo -e "${RED}Please add release notes for v$1 into CHANGELOG.md$COLOR_OFF"
     exit 1
 fi
 
-echo "Updating version numbers in plist to '$1'.."
+echo -e "${GREEN}Updating version numbers in plist to '$1'..$COLOR_OFF"
 agvtool new-version -all "$1" # CFBundleVersion
 agvtool new-marketing-version "$1" # CFBundleShortVersionString
 
 if [ "$THIS_FILE_CHANGED" -gt 0 ]; then
-    echo -e "\n\n\033[1;33mNot committing to Git, as this script isn't final. Commit it to continue\033[0m"
+    echo -e "${BOLD_YELLOW}Not committing to Git, as this script isn't final. Commit it to continue$COLOR_OFF"
     exit 2
 fi
 
-echo "Committing updated plist..."
+echo -e "${GREEN}Committing updated plist...$COLOR_OFF"
 git commit -m "Updated plist to v$1" Resources
 
 # Revert changes to other plist files
 git checkout .
 
-echo "Tagging build..."
+echo -e "${GREEN}Tagging build...$COLOR_OFF"
 git tag $1 -m "$RELEASE_NOTES"
